750eb05f363e2a2b6a5fd9e31ef599f846bf6705
Eber <doneber98@gmail.com>
2020-06-26 21:30:00 -0400

    Se agrego otro card y se cambio el click del card

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <v-row>
       <v-col md="4">
           <v-card
+          to="/poo"
             color="#385F73"
             dark
           >
@@ -12,10 +13,26 @@
             <v-card-subtitle>Programación Orientada a Objetos</v-card-subtitle>
 
             <v-card-actions>
-              <v-btn to="/poo" text>Curiosea ahora</v-btn>
+              <v-btn text>Curiosea ahora</v-btn>
             </v-card-actions>
           </v-card>
       </v-col>
+      <v-col md="4">
+          <v-card
+          disabled
+          to="/pf"
+          :color="this.$vuetify.theme.dark?'grey darken-1':'grey lighten-1'"
+          >
+            <v-card-title class="">Programación Funcional</v-card-title>
+
+            <v-card-subtitle>No disponible</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>No disponible</v-btn>
+            </v-card-actions>
+          </v-card>
+      </v-col>
+
     </v-row>
 
   </div>
