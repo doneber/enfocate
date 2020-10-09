205133c142226bc93b6c6947553aa4453222aabb
Eber <doneber98@gmail.com>
2020-10-09 12:32:16 -0400

    Deleted eDatos content & updated Drawer component

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -5,21 +5,21 @@
         v-subheader.grey--text.font-weight-black {{ items.title }}:
       v-divider
       template(v-for='(item,index) in items.items')
-        v-list-group(v-if='item.items && item.items.length > 0' :key="index+'-'+item.title+'-manylines'" :prepend-icon='item.action' no-action='' :disabled='item.block')
+        v-list-group(v-if='item.items && item.items.length > 0' :key="`${index}-${item.title}-manylines`" :prepend-icon='item.action' no-action='' :disabled='item.block')
           template(v-slot:activator='')
             v-list-item-content
               v-list-item-title(v-text='item.title')
-          v-list-item(v-for='subItem in item.items' :key='subItem.title' :to='subItem.to')
+          v-list-item(v-for='subItem in item.items' :key='`${index}-${subItem.title}-${subItem.icon}`' :to='subItem.to')
             v-list-item-icon.mr-2
               v-icon {{subItem.icon}}
             v-list-item-content
               v-list-item-title(v-text='subItem.title')
-        v-list-item(v-else='' :key="item.title+'-oneline'" :to='item.to' :disabled='item.block')
+        v-list-item(v-else='' :key="`${index}-${item.title}-oneline`" :to='item.to' :disabled='item.block')
           v-list-item-icon.mr-3
             v-icon {{item.icon}}
           v-list-item-content
             v-list-item-title(v-text='item.title')
-        v-divider(:key='item.title')
+        v-divider(:key="`${index}+${item.title}-divider`")
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
