43848bf3d4c8bd5faac53d6b24efcdcfe8f5fbdb
doneber <52986565+doneber@users.noreply.github.com>
2020-11-29 00:32:47 -0400

    Update details in app

--- components/Bar.vue
+++ components/Bar.vue
@@ -2,8 +2,8 @@
   div
     v-app-bar(app='' clipped-left='' dense='' elevation='1' height='56' dark='' color='primaryDark')
       v-app-bar-nav-icon(@click='setDrawer')
-      v-img.mr-2.mb-1(max-width='50px' src='/icon.png' @click='goHome()')
-      v-toolbar-title.align-center(style='font-weight: bold; font-size : 1.65em;' @click='goHome()' :class="[!showSearcher?'':'hidden-xs-only']") Enfocate
+      v-img.mr-2.mb-1(max-width='50px' src='/icon.png' @click='goHome()' style='cursor: pointer;')
+      v-toolbar-title.align-center(style='font-weight: bold; font-size : 1.65em; cursor: pointer;' @click='goHome()' :class="[!showSearcher?'':'hidden-xs-only']") Enfocate
       v-spacer.hidden-xs-only
       .padre
         v-text-field(clearable='' :class="[showSearcher?'':'hidden-xs-only']" placeholder='Buscar' @blur='ocultarBuscador' hide-details='' :autofocus='showSearcher' v-model='textoBuscador')
--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,20 +1,11 @@
 <template lang="pug">
   v-navigation-drawer(color='primaryMiddle' v-model='myDrawer' app='' clipped='')
-    v-list
-      v-list-item(:to='items.baseEndPoint' exact='')
-        v-subheader.grey--text.font-weight-black {{ items.title }}:
+    v-list.pt-0
+      v-list-item.pt-0(:to='items.baseEndPoint' exact='')
+        v-subheader.font-weight-bold {{ items.title }}:
       v-divider
       template(v-for='(item,index) in items.items')
-        v-list-group(v-if='item.items && item.items.length > 0' :key="`${index}-${item.title}-manylines`" :prepend-icon='item.action' no-action='' :disabled='item.block')
-          template(v-slot:activator='')
-            v-list-item-content
-              v-list-item-title(v-text='item.title')
-          v-list-item(v-for='subItem in item.items' :key='`${index}-${subItem.title}-${subItem.icon}`' :to='subItem.to')
-            v-list-item-icon.mr-2
-              v-icon {{subItem.icon}}
-            v-list-item-content
-              v-list-item-title(v-text='subItem.title')
-        v-list-item(v-else='' :key="`${index}-${item.title}-oneline`" :to='item.baseEndPoint' :disabled='item.block')
+        v-list-item(:key="`${index}-${item.title}-oneline`" :to='item.baseEndPoint' :disabled='item.block')
           v-list-item-icon.mr-3
             v-icon {{item.icon}}
           v-list-item-content
@@ -35,7 +26,7 @@ export default {
     ...mapActions(["getCourses"]),
     calculaDrawer(url) {
       /* depending of the actual route, we dispaly an especific item's list */
-      if (url === "/"  || url === "/nosotros" ) this.items = this.courses['home']
+      if (url === "/"  || url === "/nosotros" || url === "/comunidad" || url === "/contacto") this.items = this.courses['home']
       else this.items = this.courses[url.split('/')[1]]
     },
   },
