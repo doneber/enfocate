97a223c7adf849d09db5b23c1c0065dc1f45743b
doneber <52986565+doneber@users.noreply.github.com>
2020-12-01 12:15:49 -0400

    Update Comunidad page

--- pages/comunidad.vue
+++ pages/comunidad.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
   div
-    v-row.mx-0(align='center' justify='center' style="min-height:500px" class="primaryMiddle")
+    v-row.mx-0(style="min-height:15vh")
+    v-row.mx-0(align='center' justify='center' style="min-height:50vh" class="primaryMiddle")
       v-col.text-center(cols="12" sm="12")
         h1.display-3.font-weight-bold(:class="[this.$vuetify.theme.dark?'orange--text text--lighten-2':'' ]")
           | Comunidad
         h3.font-weight-light
-          | ¡Quiero colaborar con otras personas para crecer profesionalmente!
-    //- v-row.mx-0(align='center' justify='center' style="min-height:400px" )
-    //-   v-col.text-center
-    //-     p.display-1.font-weight-thin
-    //-       | .
-    //-     p.display-1.font-weight-thin
-    //-       | 
+          | Sé parte de la comunidad en <b>discord</b> y comienza a compartir los demás<br>
+        p.font-weight-light.font-italic La mejor forma de aprender es enseñando
+        v-btn.py-4(x-large text tile href="https://discord.gg/HXBhXJJ9kn" target="_blank" )
+          v-icon.mr-5(x-large right) mdi-discord
+          |  Quiero unirme!
 </template>
 <script>
 export default {
